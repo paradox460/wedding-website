2ef6cb71fa9fcabfca805ecddf31e7d92e128b7e
Jeff Sandberg <jeff.sandberg@onelogin.com>
2014-06-02 18:45:25 -0700

    Created invite model

---
+++ app/models/invite.rb
@@ -0,0 +1,10 @@
+class Invite < ActiveRecord::Base
+  before_create :random_code
+
+  private
+  def random_code
+    begin
+      self.code = rand(32**6).to_s(32)
+    end while Invite.where(code: self.code).exists?
+  end
+end
