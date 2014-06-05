b2d1bae9eca1a13878e53439677cf9a3d9a42891
Jeff Sandberg <jeff.sandberg@onelogin.com>
2014-06-04 19:00:57 -0700

    Added validations to rsvp

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,4 +1,6 @@
 class Rsvp < ActiveRecord::Base
   belongs_to :invite
-
+  validates :attending, presence: true
+  validates :adults, numericality: {greater_than_or_equal_to: 1, less_than: 10}
+  validates :children, numericality: {less_than: 10}
 end
