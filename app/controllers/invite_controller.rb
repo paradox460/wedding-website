5381b9ee9e8634f14e618a8c2dcfae80f9fcdc79
Jeff Sandberg <jeff.sandberg@onelogin.com>
2014-06-04 19:38:12 -0700

    Added robustness to invite controller

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -8,11 +8,11 @@ class InviteController < ApplicationController
 
   def accept
     @invite = Invite.where(code: params[:code]).take
-    @invite.rsvp = Rsvp.create(attending: true, adults: params[:adults], children: params[:children])
+    @invite.rsvp = Rsvp.create(attending: true, adults: params[:adults], children: params[:children]) unless @invite.rsvp.exists?
   end
 
   def reject
     @invite = Invite.where(code: params[:code]).take
-    @invite.rsvp = Rsvp.create(attending: false)
+    @invite.rsvp = Rsvp.create(attending: false) unless @invite.rsvp.exists?
   end
 end
