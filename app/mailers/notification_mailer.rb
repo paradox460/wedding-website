0cccf1d70796a46a775b162fd99ec07d0775ac4f
Jeff Sandberg <jeff.sandberg@onelogin.com>
2014-06-08 14:51:12 -0700

    Fixed rsvp mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,7 @@
 class NotificationMailer < ActionMailer::Base
   default from: "notifications@sandbergwedding.com"
 
-  def new_rsvp(rsvp, invite)
+  def new_rsvp(rsvp)
     @rsvp = rsvp
 
     mail to: '***REMOVED***', subject: "#{@rsvp.invite.name} is #{@rsvp.attending ? 'coming' : 'not coming'}"
