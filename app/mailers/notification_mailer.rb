1f2f8dc6e83cfdb464257d9acc327ed7d3be8b0e
Jeff Sandberg <jeff.sandberg@onelogin.com>
2014-06-08 15:01:25 -0700

    Made mailer send notifications to multiple emails

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,7 +4,6 @@ class NotificationMailer < ActionMailer::Base
   def new_rsvp(rsvp)
     @rsvp = rsvp
 
-    mail to: '***REMOVED***', subject: "#{@rsvp.invite.name} is #{@rsvp.attending ? 'coming' : 'not coming'}"
-    mail to: '***REMOVED***', subject: "#{@rsvp.invite.name} is #{@rsvp.attending ? 'coming' : 'not coming'}"
+    mail to: ['***REMOVED***', '***REMOVED***'], subject: "#{@rsvp.invite.name} is #{@rsvp.attending ? 'coming' : 'not coming'}"
   end
 end
