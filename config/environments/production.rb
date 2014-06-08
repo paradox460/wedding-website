dbb4518f9586cd58ee1b95ea1fb69a2ea4a6c545
Jeff Sandberg <jeff.sandberg@onelogin.com>
2014-06-08 13:47:33 -0700

    Added mailgun for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :authentication => :plain,
+    :address => "smtp.mailgun.org",
+    :port => 587,
+    :domain => "sandbergwedding.com",
+    :user_name => "postmaster@sandbergwedding.com",
+    :password => "68o96etc7nl2"
+  }
+
 end
